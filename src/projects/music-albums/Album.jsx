afe3e0dfcaa0cc1462e80828b39eecd76cd86e63
George Cornea <george.cornea@outlook.com>
2023-04-28 11:41:27 -0700

    Adjusted styling for Music Album Project

--- src/projects/music-albums/Album.jsx
+++ src/projects/music-albums/Album.jsx
@@ -3,7 +3,7 @@ import { AlbumTracks } from './AlbumTracks';
 
 export const Album = ({ album }) => {
   return (
-    <div className='flex gap-4 pt-4 px-3'>
+    <div className='flex gap-4 pt-1 px-3'>
       <AlbumCoverImage src={album.coverImg} name={album.name} />
       <AlbumTracks tracks={album.tracks} />
     </div>
--- src/projects/music-albums/MusicAlbums.jsx
+++ src/projects/music-albums/MusicAlbums.jsx
@@ -7,7 +7,7 @@ export const MusicAlbums = () => {
   const [album, setAlbum] = useState(albums[0]);
 
   return (
-    <div className='shadow-md shadow-black max-w-xl mx-auto flex flex-col justify-center text-center px-2 my-4 bg-cyan-900'>
+    <div className='shadow-md shadow-black w-[30rem] h-[38rem] text-gray-300 mx-auto flex flex-col justify-start text-center pt-4 px-2 my-4 bg-cyan-900'>
       <Album album={album} />
       <AlbumCollection albums={albums} setAlbum={setAlbum} />
     </div>
